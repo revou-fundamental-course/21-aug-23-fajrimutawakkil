0890093ec4c2a7714f31df06a1c8a4d8e5b2ae3c
fajrimutawakkil <fajrimutawakkil@gmail.com>
2023-08-31 21:37:26 +0700

    Final Code

--- js/script.js
+++ js/script.js
@@ -24,3 +24,27 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+
+function validateForm() {
+    if (document.forms["mesaggeForm"]["name"].value == "") {
+        alert("Name can't be empty.");
+        document.forms["mesaggeForm"]["name"].focus();
+        return false;
+    }
+    if (document.forms["mesaggeForm"]["email"].value == "") {
+        alert("Input your email.");
+        document.forms["mesaggeForm"]["email"].focus();
+        return false;
+    }
+    if (document.forms["mesaggeForm"]["phone"].value == "") {
+        alert("Input your phone number.");
+        document.forms["mesaggeForm"]["phone"].focus();
+        return false;
+    }
+    if (document.forms["mesaggeForm"]["message"].value == "") {
+        alert("What your thoughts?");
+        document.forms["mesaggeForm"]["message"].focus();
+        return false;
+    }
+}
